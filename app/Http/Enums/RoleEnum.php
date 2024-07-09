47a68c2d1f9f89f6bf27a005ab69da87a13e32af
ronaldPerez21 <randyperez9864@gmail.com>
2024-07-09 16:33:09 -0400

    Cambiando el tipo de enum de roles

--- app/Http/Enums/RoleEnum.php
+++ app/Http/Enums/RoleEnum.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\Http\Enums;
 
-enum RoleEnum: string {
+enum RoleEnum: int {
 
-    case Administrativo = "Administrativo" ;
-    case Proveedor = "Proveedor";
-    case Empleado = "Empleado";
-    case Cliente = "Cliente";
+    // case Administrativo = "Administrativo" ;
+    // case Proveedor = "Proveedor";
+    // case Empleado = "Empleado";
+    // case Cliente = "Cliente";
+
+    case Administrativo = 1 ;
+    case Proveedor      = 2;
+    case Empleado       = 3;
+    case Cliente        = 4;
 }
